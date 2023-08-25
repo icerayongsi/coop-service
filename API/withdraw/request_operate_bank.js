2dd96bda0c06e16075e61f36114101bc7dafb5af
dev_gensoft <developer@gensoft.co.th>
2023-08-25 16:30:53 +0700

    from pea

--- API/withdraw/request_operate_bank.js
+++ API/withdraw/request_operate_bank.js
@@ -87,12 +87,99 @@ API.post('/payment', async (req, res) => {
     }
 })
 
-API.post('/re-payment' , async (req,res) => {
-    try {
-        
-    } catch (error) {
-        
+API.post('/payment-test', async (req, res) => {
+    console.log('test')
+    const query = `
+        BEGIN
+            POST_DEPT_INSERT_SERV_ONLINE(
+                AS_BANK_CODE=>:AS_BANK_CODE,
+                AS_MEMBER_NO=>:AS_MEMBER_NO,
+                AS_DEPTACCOUNT_NO=>:AS_DEPTACCOUNT_NO ,
+                AS_MEMBCAT_CODE=>:AS_MEMBCAT_CODE,
+                AS_COOP_ID=>:AS_COOP_ID,
+                AS_DEPTCOOP_ID=>:AS_DEPTCOOP_ID,
+                AS_DEPTTYPE_CODE=>:AS_DEPTTYPE_CODE ,
+                AS_DEPTGROUP_CODE=>:AS_DEPTGROUP_CODE ,
+                AS_OPERATE_DATE=>:AS_OPERATE_DATE ,
+                AS_ENTRY_DATE=>:AS_ENTRY_DATE,
+                AS_ENTRY_ID=>:AS_ENTRY_ID ,
+                AS_OPERATE_CODE=>:AS_OPERATE_CODE,
+                AS_SLIPITEMTYPE_CODE=>:AS_SLIPITEMTYPE_CODE,
+                AS_SIGN_FLAG_=>:AS_SIGN_FLAG,
+                AS_ITEM_AMT_=>:AS_ITEM_AMT ,
+                AS_MONEYTYPE_CODE=>:AS_MONEYTYPE_CODE,
+                AS_MACHINE_ID=>:AS_MACHINE_ID,
+                AS_FEE_AMT_=>:AS_FEE_AMT ,
+                AS_TOFROMACCID=>:AS_TOFROMACCID,
+                AS_OTH_AMT_=>:AS_OTH_AMT,
+                AS_PRNCBAL_=>:AS_PRNCBAL ,
+                AS_WITHDRAWABLE_AMT_=>:AS_WITHDRAWABLE_AMT,
+                AS_LASTSTMSEQ_NO_=>:AS_LASTSTMSEQ_NO ,
+                AS_ACTION_STATUS_=>:AS_ACTION_STATUS,
+                AS_POST_STATUS_=>:AS_POST_STATUS,
+                AS_DEPTSLIP_NO=>:AS_DEPTSLIP_NO,
+                AS_DEPTSLIP_NO_FEE=>:AS_DEPTSLIP_NO_FEE,
+                AS_DEPTSLIP_NO_OTH=>:AS_DEPTSLIP_NO_OTH,
+                AS_PROCESS_STATUS=>:AS_PROCESS_STATUS
+            );
+        END;`
+
+    const model = {
+        AS_BANK_CODE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 3, val: '006' },
+        AS_MEMBER_NO: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 8, val: '00262156' },
+        AS_DEPTACCOUNT_NO: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 20, val: '0100260930' },
+        AS_MEMBCAT_CODE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 6, val: '10' },
+        AS_COOP_ID: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 6, val: '008001' },
+        AS_DEPTCOOP_ID: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 10, val: '008001' },
+        AS_DEPTTYPE_CODE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 2, val: '00' },
+        AS_DEPTGROUP_CODE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 5, val: '00' },
+        AS_OPERATE_DATE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 50, val: '2023-08-24 11:56:42' },
+        AS_ENTRY_DATE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 50, val: '08/24/2023 16:53:50' },
+        AS_ENTRY_ID: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 50, val: 'MOBILE' },
+        AS_OPERATE_CODE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 8, val: '' },
+        AS_SLIPITEMTYPE_CODE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 4, val: 'WTX' },
+        AS_SIGN_FLAG: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 10, val: '-1' },
+        AS_ITEM_AMT: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 20, val: '15.5' },
+        AS_MONEYTYPE_CODE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 4, val: 'CBT' },
+        AS_MACHINE_ID: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 30, val: 'K0151900006835' },
+        AS_FEE_AMT: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 20, val: '100' },
+        AS_TOFROMACCID: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 10, val: '11040100' },
+        AS_OTH_AMT: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 20, val: '0' },
+        AS_PRNCBAL: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 20, val: '904685.5' },
+        AS_WITHDRAWABLE_AMT: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 20, val: '904685.5' },
+        AS_LASTSTMSEQ_NO: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 10, val: '220' },
+        AS_ACTION_STATUS: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 5, val: '1' },
+        AS_POST_STATUS: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 2, val: '1' },
+        AS_DEPTSLIP_NO: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 30, val: '' },
+        AS_DEPTSLIP_NO_FEE: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 30, val: '' },
+        AS_DEPTSLIP_NO_OTH: { dir: oracledb.BIND_INOUT, type: oracledb.STRING, maxSize: 30, val: '' },
+        AS_PROCESS_STATUS: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 3000, val: '' }
     }
+    console.log("ก่อนเรียก PL/SQL", new Date())
+    await oracleExecute(query, model)
+        .then(async (result) => {
+            console.log("หลังเรียก PL/SQL", new Date())
+            console.log(result.outBinds)
+            res.status(200).json(result.outBinds)
+            res.end()
+        })
+        .catch(async (err) => {
+            console.log(err)
+        })
 })
 
+API.post('/payment-test-', async (req, res) => {
+    console.log('in')
+    const query = `
+        BEGIN
+            POST_DEPT_INSERT_SERV_TEST(
+                AS_NUMBER => :AS_NUMBER,
+                AS_DATE => :AS_DATE
+            );
+        END;
+    `
+    const result = await oracleExecute(query, POST_DEPT_INSERT_TEST())
+    console.log(result.outBinds)
+    res.json(result.outBinds)
+})
 export default API
